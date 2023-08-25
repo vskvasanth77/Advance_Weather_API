ffec0af298a498f8e96b3a4e4211e4664cd00b11
Vasanth <vskvasanth777@gmail.com>
2023-08-25 18:25:02 +0530

    Still in development process

--- src/service/WeatherService.js
+++ src/service/WeatherService.js
@@ -54,7 +54,7 @@ const formatForecastWeather = (data) => {
   let { timezone, daily, hourly } = data;
   daily = daily.slice(1, 6).map((d) => {
     return {
-      title: formatToLocalTime(d.dt, timezone, "cccc"),
+      title: formatToLocalTime(d.dt, timezone, "ccc"),
       temp: d.temp.day,
       icon: d.weather[0].icon,
     };
